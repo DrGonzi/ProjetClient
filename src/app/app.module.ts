eb42b8d98897a0a19d58aecf93fb507671caa910
DrGonzi <thomas.gonzi440@gmail.com>
2018-11-16 09:33:40 +0100

    ajout des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CoachingComponent } from './coaching/coaching.component';
+import { MassageComponent } from './massage/massage.component';
+import { WhoAmIComponent } from './who-am-i/who-am-i.component';
+import { ContactComponent } from './contact/contact.component';
+import { RdvComponent } from './rdv/rdv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AccueilComponent,
+    CoachingComponent,
+    MassageComponent,
+    WhoAmIComponent,
+    ContactComponent,
+    RdvComponent
   ],
   imports: [
     BrowserModule
