00ea61bc46b130c0ea3ce23032c5c09a9ed574ab
Damien Heulin <damienheulin87@hmail.com>
2018-11-20 14:07:19 +0100

    Changed components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { AccueilComponent } from './accueil/accueil.component';
-import { CoachingComponent } from './coaching/coaching.component';
-import { MassageComponent } from './massage/massage.component';
-import { WhoAmIComponent } from './who-am-i/who-am-i.component';
-import { ContactComponent } from './contact/contact.component';
-import { RdvComponent } from './rdv/rdv.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { CoachingComponent } from './components/coaching/coaching.component';
+import { MassageComponent } from './components/massage/massage.component';
+import { WhoAmIComponent } from './components/who-am-i/who-am-i.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RdvComponent } from './components/rdv/rdv.component';
 
 @NgModule({
   declarations: [
